a349a124fff2f68a4776315ac90d7aeb8d69fb1c
ABRONA <abrahamromero202@gmail.com>
2024-04-04 00:10:47 -0600

    assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -3,6 +3,16 @@ import logo from './logo.svg'
 import coppel_emprende from './coppel-emprende.png'
 import maryFernandez from './maryFernandez.png'
 import floreria from './floreria.jpg'
+import alePerPP from './alejandroPerezPP.png'
+import anaGarPP from './anaGarciaPP.png'
+import carMtzPP from './carlosMtzPP.png'
+import ecoClePP from './ecoCleanPP.png'
+import lolPP from './lolaPP.png'
+import lunPP from './lunaPP.png'
+import marFerPP from './mariaFernandaPP.png'
+import matEquPP from './materialsEquipmentPP.png'
+import sabUrbPP from './saborUrbanoPP.png'
+import sunPP from './sunnyPP.png'
 
 export {
     full_logo,
@@ -10,4 +20,14 @@ export {
     coppel_emprende,
     maryFernandez,
     floreria,
+    alePerPP,
+    anaGarPP,
+    carMtzPP,
+    ecoClePP,
+    lolPP,
+    lunPP,
+    marFerPP,
+    matEquPP,
+    sabUrbPP,
+    sunPP
 }
